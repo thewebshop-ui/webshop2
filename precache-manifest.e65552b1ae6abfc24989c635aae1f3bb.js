29b3305f3a1a1805517555bc9a05a3e1522be505
thewebshop-ui <thewebshop2@gmail.com>
2025-06-09 13:20:44 +0200

    Updates

---
+++ precache-manifest.e65552b1ae6abfc24989c635aae1f3bb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50f181704f70dd070a2e8964f895513d",
+    "url": "/webshop2/index.html"
+  },
+  {
+    "revision": "482cf8f6285aaf9c41eb",
+    "url": "/webshop2/static/css/main.c14068c8.chunk.css"
+  },
+  {
+    "revision": "c2fc3fa368d94af51e5f",
+    "url": "/webshop2/static/js/2.357ea4c0.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/webshop2/static/js/2.357ea4c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "482cf8f6285aaf9c41eb",
+    "url": "/webshop2/static/js/main.e445d203.chunk.js"
+  },
+  {
+    "revision": "1e448263bfa4b556260f",
+    "url": "/webshop2/static/js/runtime-main.6b67b4f5.js"
+  },
+  {
+    "revision": "cf97d009d0517c9419b4deda3e517039",
+    "url": "/webshop2/static/media/Ernstings.cf97d009.png"
+  },
+  {
+    "revision": "28a304acd288123d6da098094bde1811",
+    "url": "/webshop2/static/media/icon.28a304ac.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/webshop/precache-manifest.05f46c16f0d717cee827e2b5eda7b8e7.js"
+  "/webshop2/precache-manifest.e65552b1ae6abfc24989c635aae1f3bb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/webshop/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/webshop2/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
